527711f875608b26265dd00c9c8abb05a3a97f60
ggn <ggn.dbug@gmail.com>
2020-04-04 21:50:08 +0300

    Fix long standing issue with compiling basic_functions.c using rb+'s gcc. Now bootstrapping the build object files should be possible (provided I didn't screw anything up)

--- include/build_basic_functions.bat
+++ include/build_basic_functions.bat
@@ -1,5 +1,4 @@
-@rem ee_printf WILL crash without -O2, so take care!
-@PATH=%PATH%;..\bin
-m68k-atari-mint-gcc basic_functions.c -c -O2 -I. -I..\include -fcall-saved-a1 -fcall-saved-d1
-m68k-atari-mint-gcc basic_functions.c -S -O2 -I. -I..\include -fcall-saved-a1 -fcall-saved-d1
-m68k-atari-mint-gcc ee_printf.c -c -O3 -I. -I..\include
+REM rem ee_printf WILL crash without -O2, so take care!
+..\BIN\m68k-atari-mint-gcc basic_functions.c -c -O2 -I. -Iinclude -fcall-saved-a1 -fcall-saved-d1
+..\BIN\m68k-atari-mint-gcc basic_functions.c -S -O2 -I. -Iinclude -fcall-saved-a1 -fcall-saved-d1
+..\BIN\m68k-atari-mint-gcc ee_printf.c -c -O3 -I. -Iinclude
---
+++ include/build_basic_functions.sh
@@ -0,0 +1,4 @@
+# ee_printf WILL crash without -O2, so take care!
+m68k-atari-mint-gcc basic_functions.c -c -O2 -I. -I..\include -fcall-saved-a1 -fcall-saved-d1
+m68k-atari-mint-gcc basic_functions.c -S -O2 -I. -I..\include -fcall-saved-a1 -fcall-saved-d1
+m68k-atari-mint-gcc ee_printf.c -c -O3 -I. -I..\include
